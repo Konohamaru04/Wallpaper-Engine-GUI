d61c3b6466dce8470ec18b9f0522939ffeeeab4a
Konohamaru04 <dedsec.konohamaru@gmail.com>
2020-04-12 00:44:39 +0530

    v1.1
    v1.1

---
+++ app/js/sidenavMenu.js
@@ -0,0 +1,9 @@
+function openSlideMenu(){
+    document.getElementById('menu').style.width = '250px';
+    document.getElementById('content').style.marginLeft = '250px';
+}
+
+function closeSlideMenu(){
+    document.getElementById('menu').style.width = '0px';
+    document.getElementById('content').style.marginLeft = '0px';
+}
